66b07f476d4853ae7b6b614229c0d4af8ebf8e9f
Marco Rodriguez <marco.rodriguez113@gmail.com>
2022-01-25 15:31:31 -0800

    added addToIPFS, verified CreateEntryWithEmbed, working on PublishToIPNS

--- server.go
+++ server.go
@@ -13,29 +13,49 @@ import (
 	"net/http"
     "time"
 	"io"
+	// "encoding/hex"
+	"os"
     // "encoding/json"
 
-    "github.com/gorilla/mux"
+    // "github.com/gorilla/mux"
+	
 	shell "github.com/ipfs/go-ipfs-api"
     pb "github.com/ipfs/go-ipns/pb"
     ipns "github.com/ipfs/go-ipns"
     crypto "github.com/libp2p/go-libp2p-core/crypto"
 )
 
-const hash = "Qmbyuv31VFiD9gpq6FaugFiwdcnDVsuyfxrXuu65kxEzMy"
-const ipfsPath = "/ipfs/Qmbyuv31VFiD9gpq6FaugFiwdcnDVsuyfxrXuu65kxEzMy"
-
+const filepath = "Hello"
+const ipfsPath = "/ipfs/QmSVjCYjy4jYZynyC2i5GeFgjhq1bLCK2vrkRz5ffnssqo"
+const localhost = "localhost:5001"
 
 func index(w http.ResponseWriter, r *http.Request){
     fmt.Fprintf(w, "Welcome to the HomePage!")
     fmt.Println("Endpoint Hit: homePage")
 }
 
-// This function is needed to let the world know your Record exists.
-func PublishToIPNS(ipfsPath string, privateKey crypto.PrivKey) {
-    shell := shell.NewShell("localhost:5001")
+// Correct! Adds file to IPFS, given path/filename,
+// returns CID
+func addToIPFS(file string) {
+	sh := shell.NewShell(localhost)
+	fileReader, err := os.Open(file)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %s ", err)
+		os.Exit(1)
+	}
+	cid, err:= sh.Add(fileReader)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %s ", err)
+		os.Exit(1)
+	}
+	fmt.Printf("added %s\n", cid)
+}
 
-    resp, err := shell.PublishWithDetails(ipfsPath, "", time.Second, time.Second, false)
+// This function is needed to let the world know your Record exists.\
+// May not be correct procedure
+func PublishToIPNS(ipfsPath string, privateKey string) {
+	sh := shell.NewShell(localhost)
+    resp, err := sh.PublishWithDetails(ipfsPath, privateKey, time.Second, time.Second, false)
     if err != nil {
 		log.Fatal(err)
 	}
@@ -43,10 +63,8 @@ func PublishToIPNS(ipfsPath string, privateKey crypto.PrivKey) {
 	if resp.Value != ipfsPath {
 		fmt.Sprintf("Expected to receive %s but got %s", ipfsPath, resp.Value)
 	}
-	fmt.Println("response name: %s\nresponse value: %s", resp.Name, resp.Value)
+	fmt.Println("response value: %s\n", resp.Value)
 }
-
-
 
 // CreateEntryWithEmbed shows how you can create an IPNS entry
 // and embed it with a public key. For ed25519 keys this is not needed
@@ -65,7 +83,6 @@ func CreateEntryWithEmbed(ipfsPath string, privateKey crypto.PrivKey) (*pb.IpnsE
 
 	return entry, nil
 }
-
 
 // Create IPNS entry & embed Public key into entry, & upload to IPFS return entry to enter in contract. 
 // ipfsPath string, privkey crypto.PrivKey
@@ -119,35 +136,38 @@ func getKey(w http.ResponseWriter, r *http.Request) {
 func testFunctions() {
 	fmt.Println("Creating key:pair...")
 	privKey, _, err := crypto.GenerateKeyPair(crypto.RSA, 2048)
+	// rawKey, err := privKey.Raw()
+	// hexkey := hex.EncodeToString(rawKey)
+	// fmt.Printf("hex pub key: %s\n", hexkey)
     // verify there was no error
 	if err != nil {
         panic(err)
     }
 
-	fmt.Println("Creating IPNS record...")
+	fmt.Println("Creating IPNS record...", privKey)
 	ipnsRecord, err := CreateEntryWithEmbed(ipfsPath, privKey)
-    // Verify there was no error
-	if err != nil {
-	    panic(err)
-    }
+    // // Verify there was no error
+	// if err != nil {
+	//     panic(err)
+    // }
 
-	fmt.Println("Publishing to IPFS...")
-	PublishToIPNS(ipfsPath, privKey)
+	fmt.Sprint(ipnsRecord)
+	// PublishToIPNS(ipfsPath, hexkey)
 
-
-	fmt.Printf("functions individually work. %s\n\n", ipnsRecord)
+	fmt.Printf("functions individually work.\n\n")
 }
 
 func main() {
 	// handles api/website routes.
 	// Used to test if keys need to be passed as objects or ints?
+	addToIPFS(filepath)
 	testFunctions()
 
-	router := mux.NewRouter().StrictSlash(true)
-    router.HandleFunc("/", index)
-	router.HandleFunc("/getKey", getKey).Methods("GET")
-	router.HandleFunc("/postKey", postKey).Methods("POST")
+	// router := mux.NewRouter().StrictSlash(true)
+    // router.HandleFunc("/", index)
+	// router.HandleFunc("/getKey", getKey).Methods("GET")
+	// router.HandleFunc("/postKey", postKey).Methods("POST")
 
-	fmt.Printf("Starting server at port 8082\n")
-	log.Fatal(http.ListenAndServe(":8082", router))
+	// fmt.Printf("Starting server at port 8082\n")
+	// log.Fatal(http.ListenAndServe(":8082", router))
 }
