5c01b5ad08c2772cbf874289d5165a2f2ffe300b
Marco Rodriguez <marco.rodriguez113@gmail.com>
2022-02-10 16:29:11 -0800

    Added example GET & PUT request

--- requests.sh
+++ requests.sh
@@ -1,3 +1,6 @@
 #!/bin/bash 
 
-curl http://localhost:8082/postKey?CID=QmSVjCYjy4jYZynyC2i5GeFgjhq1bLCK2vrkRz5ffnssqo -F "file=@temp.key" -vvv
+curl --request GET http://localhost:8082/getKey 
+
+curl --request POST http://localhost:8082/postKey?CID=QmSVjCYjy4jYZynyC2i5GeFgjhq1bLCK2vrkRz5ffnssqo -F "file=@temp.key" -vvv
+
