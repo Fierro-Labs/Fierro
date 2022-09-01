9771e4d27cdff37c40954d2b64003c7228f72069
Marco Rodriguez-Salinas <marco.rodriguez113@gmail.com>
2022-08-31 23:32:59 -0700

    Added pin objects & refactoring code

---
+++ src/pin_objects.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"time"
+)
+
+// Pin object with status
+type PinStatus struct {
+	// Globally unique identifier of the pin request; can be used to check the status of ongoing pinning, or pin removal
+	Requestid string `json:"requestid"`
+
+	Status *Status `json:"status"`
+	// Immutable timestamp indicating when a pin request entered a pinning service; can be used for filtering results and pagination
+	Created time.Time `json:"created"`
+
+	Pin *Pin `json:"pin"`
+
+	Delegates *[]string `json:"delegates"`
+
+	Info *map[string]string `json:"info,omitempty"`
+}
+
+// Pin object
+type Pin struct {
+	// Content Identifier (CID) to be pinned recursively
+	Cid string `json:"cid"`
+	// Optional name for pinned data; can be used for lookups later
+	Name string `json:"name,omitempty"`
+
+	Origins *[]string `json:"origins,omitempty"`
+
+	Meta *map[string]string `json:"meta,omitempty"`
+}
+
+// Status : Status a pin object can have at a pinning service
+type Status string
+
+// List of Status
+const (
+	QUEUED  Status = "queued"
+	PINNING Status = "pinning"
+	PINNED  Status = "pinned"
+	FAILED  Status = "failed"
+)
--- src/utils.go
+++ src/utils.go
@@ -9,27 +9,23 @@ import (
 )
 
 // This function will set the appropiate headers for when there is an error.
-func writeJSONError(w http.ResponseWriter, msg string, err error) {
-	resp := make(map[string]interface{})
-	resp["message"] = msg
-	resp["error"] = err
-	jsonResp, err := json.Marshal(resp)
+func writeJSONError(w http.ResponseWriter, Pin PinStatus) {
+	jsonResp, err := json.Marshal(Pin)
 	if err != nil {
 		log.Fatalf("Error in JSON marshal. Err: %s", err.Error())
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
 	w.WriteHeader(http.StatusInternalServerError)
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(jsonResp)
 }
 
 // This function will set the appropiate headers for when there is an error.
-func writeJSONSuccess(w http.ResponseWriter, msg string, val string) {
-	resp := make(map[string]string)
-	resp["message"] = msg
-	resp["value"] = val
-	jsonResp, err := json.Marshal(resp)
+func writeJSONSuccess(w http.ResponseWriter, Pin PinStatus) {
+
+	jsonResp, err := json.Marshal(Pin)
 	if err != nil {
 		log.Fatalf("Error in JSON marshal. Err: %s", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -42,7 +38,7 @@ func writeJSONSuccess(w http.ResponseWriter, msg string, val string) {
 
 // This function grabs the specified parameter value out the URL
 // returns true or false if `parameter` exists
-func HasParam(r *http.Request, parameter string) (string, bool) {
+func hasParam(r *http.Request, parameter string) (string, bool) {
 	vars := mux.Vars(r)
 	category := vars[parameter]
 	if category == "" {
